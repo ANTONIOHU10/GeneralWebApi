2a16dc184e6fb6650a6efc308624febf07e61cfb
ANNN <10717798@polimi.it>
2025-07-11 15:32:25 +0200

    InitStructure

---
+++ GeneralWebApi/Backend/src/1-Presentation/GeneralWebApi.WebApi/Program.cs
@@ -0,0 +1,44 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi(options =>
+{
+    // add document transformer to modify the document before it is returned
+    options.AddDocumentTransformer((document, context, _) =>
+    {
+        document.Info = new()
+        {
+            Title = "Product Catalog API",
+            Version = "v1",
+            Description = """
+                Modern API for managing product catalogs.
+                """
+        };
+        return Task.CompletedTask;
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //open api expose document: https://localhost:7297/openapi/v1.json
+    app.MapOpenApi();
+
+    //scalar api reference: https://localhost:7297/scalar/v1
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
