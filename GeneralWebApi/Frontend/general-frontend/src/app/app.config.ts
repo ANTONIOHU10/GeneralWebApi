896e57fb2fd566ef00ae2c50712e265c13653749
ANNN <10717798@polimi.it>
2025-10-04 23:45:30 +0200

    feat: angular material installing

--- GeneralWebApi/Frontend/general-frontend/src/app/app.config.ts
+++ GeneralWebApi/Frontend/general-frontend/src/app/app.config.ts
@@ -3,9 +3,21 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+//import angular material
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
+  // global dependencies injection for app.component.ts 
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    //provide angular material
+
+    // provide animations for angular material, async loading
+    provideAnimationsAsync(), 
+    
+    // provide http client for angular
+    provideHttpClient(),
   ],
 };
