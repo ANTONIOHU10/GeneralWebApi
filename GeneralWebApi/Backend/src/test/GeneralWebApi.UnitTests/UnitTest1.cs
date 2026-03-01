57bd26cb56826b47c4a981285cdb6379814ecafa
ANNN <10717798@polimi.it>
2026-03-01 22:34:34 +0100

    Feat: demo test

--- GeneralWebApi/Backend/src/test/GeneralWebApi.UnitTests/UnitTest1.cs
+++ GeneralWebApi/Backend/src/test/GeneralWebApi.UnitTests/UnitTest1.cs
@@ -1,10 +1,55 @@
-﻿namespace GeneralWebApi.UnitTests;
+﻿using FluentAssertions;
+
+namespace GeneralWebApi.UnitTests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Test1_ShouldPass_WhenBasicAssertionIsTrue()
     {
+        // Arrange
+        var expected = true;
+        var actual = true;
 
+        // Act
+        // (No action needed for this simple test)
+
+        // Assert
+        Assert.True(actual);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2_ShouldPass_WhenUsingFluentAssertions()
+    {
+        // Arrange
+        var value = 42;
+        var text = "Hello World";
+
+        // Act
+        // (No action needed for this simple test)
+
+        // Assert
+        value.Should().Be(42);
+        value.Should().BeGreaterThan(0);
+        text.Should().NotBeNullOrEmpty();
+        text.Should().Contain("World");
+    }
+
+    [Fact]
+    public void Test3_ShouldPass_WhenComparingValues()
+    {
+        // Arrange
+        var number1 = 10;
+        var number2 = 20;
+        var sum = number1 + number2;
+
+        // Act
+        var result = sum;
+
+        // Assert
+        result.Should().Be(30);
+        result.Should().BeGreaterThan(number1);
+        result.Should().BeGreaterThan(number2);
     }
 }
